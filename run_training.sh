e476841021b4b1be7e48f9254439e9794b553339
Nicolas Romeo <nromeo@midway3-login3.rcc.local>
2024-04-17 11:11:23 -0500

    fixed gpu device handling

--- run_training.sh
+++ run_training.sh
@@ -9,12 +9,11 @@
 # TO USE RTX600 specify --constraint=rtx6000
 #SBATCH --constraint=v100   # constraint job runs on V100 GPU use
 #SBATCH -c 1                     # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 #SBATCH --account=pi-dinner
 
-conda activate ./env/torch
+#  conda activate ./env/cuda
 module load hdf5
-module load cuda/10.1
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 python train.py
--- train.py
+++ train.py
@@ -17,14 +17,25 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('Training')
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+if torch.cuda.is_available():
+    logger.info("CUDA is available and is being used.")
+else:
+    logger.info("CUDA is unavailable. Reverting to CPU.")
+
 def rotate_sol_tensor(trajectories, angle):
     # trajectories has size (num trajectories) x (num sample points) x 2
-    R = torch.tensor([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]], dtype=trajectories.dtype).T
+    R = torch.tensor([[torch.cos(angle), -torch.sin(angle)], [torch.sin(angle), torch.cos(angle)]],
+                     dtype=trajectories.dtype,
+                     device=device
+                    ).T
     return torch.tensordot(trajectories, R, dims=([2],[1]))
 
 def rescale_sol_tensor(trajs, scales):
     # trajectories has size (num trajectories) x (num sample points) x 2
-    R = torch.tensor([[1. + scales[0], 0.],[0., 1. + scales[1]]], dtype=trajs.dtype).T
+    R = torch.tensor([[1. + scales[0], 0.],[0., 1. + scales[1]]],
+                     dtype=trajs.dtype,
+                    device=device).T
     return torch.tensordot(trajs, R, dims=([2],[1]))
 
 def translate_sol_tensor(trajectories, shift):
@@ -38,25 +49,21 @@ def shuffleIC(traj):
     return traj[torch.randperm(traj.size()[0]), :, :]
 
 def augment_linear(traj):
-    scales = torch.randn(2)
-    transl = torch.randn(2)
-    angles = 2*np.pi*torch.rand(1)
+    scales = torch.randn(2, device=device)
+    transl = torch.randn(2, device=device)
+    angles = 2*np.pi*torch.rand(1, device=device)
     return shuffleIC(translate_sol_tensor(rescale_sol_tensor(rotate_sol_tensor(traj, angles), scales), transl))
 
 
 if __name__ == '__main__':
-    n_epoch = int(1e3)
-    batch_size = 50
-    cur_folder = "./test100cpu"
+    n_epoch = int(7e4)
+    batch_size = 1000
+    cur_folder = "./train1e4_gpu_b1e3_lr4_linear"
     
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    if torch.cuda.is_available():
-        logger.info("CUDA is available and is being used.")
-    else:
-        logger.info("CUDA is unavailable. Reverting to CPU.")
-
+    
+    #device=torch.device("cuda:0")
     # load data
-    filename = "datagen_fhn_pool_100.hdf5"
+    filename = "datagen_fhn_pool_10000.hdf5"
     dataset = DynsysDataset(filename, transform=None)
 
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -71,22 +78,23 @@ if __name__ == '__main__':
 
 
     pbar = tqdm(total=n_epoch, file=open(os.devnull, 'w'))
-    LOG_INTERVAL = n_epoch // 20
+    LOG_INTERVAL = n_epoch // 100
 
     losses = []
     rng = np.random.default_rng()
     for i in range(n_epoch):
         # sample batch_size dynamical system
-        sampled_sys, _ = next(iter(dataloader))
+        sampled_sys_cpu, _ = next(iter(dataloader))
+        sampled_sys = sampled_sys_cpu.to(device)
         list_x1s, list_x2s = [], []
         #print("sampled_sys shape:", sampled_sys.shape, sampled_sys.dtype)
         for sys in range(sampled_sys.shape[0]):
-            x1 = augment_linear(torch.squeeze(sampled_sys[sys,:,:,:]).to(device))
-            x2 = augment_linear(torch.squeeze(sampled_sys[sys,:,:,:]).to(device))
+            x1 = augment_linear(torch.squeeze(sampled_sys[sys,:,:,:]))
+            x2 = augment_linear(torch.squeeze(sampled_sys[sys,:,:,:]))
             list_x1s.append(x1)
             list_x2s.append(x2)
-        x1 = torch.flatten(torch.stack(list_x1s, dim=0),start_dim=1)
-        x2 = torch.flatten(torch.stack(list_x2s, dim=0),start_dim=1)
+        x1 = torch.flatten(torch.stack(list_x1s, dim=0),start_dim=1)#.to(device)
+        x2 = torch.flatten(torch.stack(list_x2s, dim=0),start_dim=1)#.to(device)
         
         # feed data and backprop
         model.optimizer.zero_grad()
